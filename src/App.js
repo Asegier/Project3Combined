92829823c47efe8c9a828215129dec27d0043e80
Chris Chan <cjaysea@gmail.com>
2017-04-03 10:28:21 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './common/header'
+import Recipes from './components/recipe'
+import Api from './api/api'
+
+class App extends Component {
+
+    state = {
+        recipes: [],
+        search: ""
+    };
+
+    componentDidMount(){
+        Api("apple")
+            .then(ingredients => this.setState({recipes: ingredients}))
+    }
+    handleInputChange = (e) => {
+        e.preventDefault();
+        this.setState({search: e.target.value})
+    };
+    handleSearch = () => {
+        console.log(this.state.search);
+        Api(this.state.search)
+            .then(ingredients => this.setState({recipes: ingredients}))
+    };
+  render() {
+
+    return (
+      <div>
+        <Header
+            handleInputChange={this.handleInputChange}
+            value={this.state.search}
+            handleSearch={this.handleSearch}
+        />
+        <Recipes recipes={this.state.recipes}/>
+      </div>
+    );
+  }
+}
+
+export default App;
